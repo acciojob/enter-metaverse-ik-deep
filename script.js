a87384ab03fb19d0e18746cef76c7797c85d060e
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-12 09:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+ // Get references to the <p> tag, the button, and the <h1> tag
+    const paragraph = document.getElementById("status");
+    const button = document.getElementById("enterBtn");
+    const h1Output = document.getElementById("output");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function () {
+      // Change the text inside the <p> tag
+      paragraph.textContent = "Entered Metaverse";
+
+      // Wrap the updated text in an <h1> tag and set it as the content of the <h1> tag
+      h1Output.textContent = paragraph.textContent;
+    });
