b375ca6f49e92282110d5c5514888097ac7340d1
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-12 09:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
- // Get references to the <p> tag, the button, and the <h1> tag
-    const paragraph = document.getElementById("status");
-    const button = document.getElementById("enterBtn");
-    const h1Output = document.getElementById("output");
-
-    // Add a click event listener to the button
-    button.addEventListener("click", function () {
-      // Change the text inside the <p> tag
-      paragraph.textContent = "Entered Metaverse";
-
-      // Wrap the updated text in an <h1> tag and set it as the content of the <h1> tag
-      h1Output.textContent = paragraph.textContent;
-    });
+//your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+let ptag = document.getElementById("status");
+function changeText(){
+	let h1tag = document.createElement("h1");
+	h1tag.innerText="Entered Metaverse";
+	ptag.appendChild(h1tag);
+}
